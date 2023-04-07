e22d54de742ed2bbef6cb780b0e91a95585d7403
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-07 10:03:02 +0100

    add icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 
 function App() {
 return (
   <div className="App">
     <div className="header_logo">
+      <StorefrontIcon />
 
     </div>
     <div className="header_search">
@@ -19,7 +22,7 @@ return (
         <span className="nav_itemLineTwo">Shop</span>
       </div>
       <div className="nav-item">
-        <span className="nav_itemLineOne">Basket</span>
+       <ShoppingBasketIcon fontSize='large'/>
         <span className="nav_itemLineTwo">0</span>
       </div>
     </div>

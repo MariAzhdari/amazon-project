c46008e199dfd819b7dd954b32a77d90972b817a
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-10 19:48:09 +0100

    product.js done

--- src/Product.js
+++ src/Product.js
@@ -7,13 +7,15 @@ function Product(){
         <div className="product">
           <div className="product_info">
             <p>Title</p>
-            <p className="product_price">£30</p>
+            <p className="product_price">
+              <small>£</small>
+              <strong>30</strong>
+              </p>
             <div className="product_rating">⭐⭐</div>
           </div>
           <img
             src="https://media.istockphoto.com/id/1014076920/photo/luxury-white-leather-female-bag.jpg?s=612x612&w=0&k=20&c=ARsYnN1fqn2eQVAjT4TprPlTE6OpF3EJCY84oloASbU="
             alt="product_image"
-            className="product_image"
           />
           <button>add to basket</button>
         </div>

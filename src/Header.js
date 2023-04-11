2fe2d8707d943d6fa5a8cef3c02d5751c1df6ac1
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-11 17:19:37 +0100

    React Routing

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import { Link } from "react-router-dom";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import SearchIcon from "@mui/icons-material/Search";
@@ -7,10 +8,12 @@ import SearchIcon from "@mui/icons-material/Search";
 function Header(){
     return (
       <div className="header">
-        <div className="header_logo">
-          <StorefrontIcon className="header_logoImage" fontSize="large" />
-          <h2 className="header_logoTitle">eShop</h2>
-        </div>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="header_logo">
+            <StorefrontIcon className="header_logoImage" fontSize="large" />
+            <h2 className="header_logoTitle">eShop</h2>
+          </div>
+        </Link>
         <div className="header_search">
           <input type="text" className="header_searchInput" />
           <SearchIcon className="header_searchIcon" />
@@ -24,10 +27,12 @@ function Header(){
             <span className="nav_itemLineOne">Your</span>
             <span className="nav_itemLineTwo">Shop</span>
           </div>
-          <div className="itemBasket">
-            <ShoppingBasketIcon />
-            <span className="nav_itemLineTwo   nav_basketCount">0</span>
-          </div>
+          <Link to="/Checkout" style={{ textDecoration: "none" }}>
+            <div className="itemBasket">
+              <ShoppingBasketIcon />
+              <span className="nav_itemLineTwo   nav_basketCount">0</span>
+            </div>
+          </Link>
         </div>
       </div>
     );

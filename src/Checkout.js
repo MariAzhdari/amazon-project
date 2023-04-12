5776cc54d3bf5b4b3f1ad25688f4b964bd79e1f8
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-12 10:03:42 +0100

    add CheckoutProduct

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,10 +6,15 @@ function Checkout(){
       <div className="checkout">
         <div className="checkout_left">
           <img
-            src="https://media.istockphoto.com/id/1309742752/photo/magnifying-glass-in-the-laptop-in-the-dark.jpg?s=612x612&w=0&k=20&c=osASR3SzVZfxL3W3EyHBc3SgoapOl1WHm-kwo0tVQ74="
+            src="https://media.istockphoto.com/id/1153201559/photo/star-ranking.jpg?s=612x612&w=0&k=20&c=5LvJcDLHWgzHMybwgrQZqP0T2Xq1JkfRARJ_g-LTCKk="
             alt=""
             className="checkout_ad"
           />
+          <div>
+            <h2 className="checkout_title">Your Shopping basket
+             </h2>
+             <CheckoutProduct/>
+          </div>
         </div>
         <div className="checkout_right"></div>
       </div>
